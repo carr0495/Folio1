f58c00234462387a89b2a431b4b6d136aca6b1f9
carr0495@algonquinlive.com <carr0495@algonquinlive.com>
2021-03-07 11:07:35 -0500

    added icons for pages in tab bar w focused functionality

--- src/navigation/MainNavigation.tsx
+++ src/navigation/MainNavigation.tsx
@@ -2,13 +2,39 @@ import React, { FC } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home, Profile, Post, Messages, Favorites } from "../screens";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const App: FC = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "person-circle" : "person-circle-outline";
+            } else if (route.name === "Post") {
+              iconName = focused ? "camera" : "camera-outline";
+            } else if (route.name === "Favorites") {
+              iconName = focused ? "heart-circle" : "heart-circle-outline";
+            } else if (route.name === "Messages") {
+              iconName = focused ? "chatbubbles" : "chatbubbles-outline";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#6D319B",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Profile" component={Profile} />
         <Tab.Screen name="Post" component={Post} />
